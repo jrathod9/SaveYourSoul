4f3410b059a929902cbfa8ba293c0603833f77ed
jrathod9 <lewisburnard@gmail.com>
2019-02-06 16:26:57 +0530

    colors and velocity direction updated

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight-25;
 var c = canvas.getContext('2d');
 
 var life = 100;
-var enemyColor = 'tomato';
+var enemyColor = 'black';
 var playerColor = 'teal';
 var maxEnemies = 100;
 var level = 1;
@@ -50,13 +50,17 @@ var game = {
         var i = 0;
         for(i=0;i<maxEnemies;i++){
             var temp = new enemy(
-                    Math.random()*canvas.width+canvas.width,
-                    Math.random()*canvas.height,
+                    Math.random()*2*canvas.width,
+                    Math.random()*canvas.height-canvas.height,
                     Math.random()*(-5)-2,
                     0,
                     Math.random()*3+3,enemyColor
                 );
+            temp.vy = -1*temp.vx;
             enemies.push(temp);    
+        }
+        for(i=0;i<maxEnemies;i++){
+            enemies[i].vy = -1*enemies[i].vx;
         }
         p = new player(mousex,mousey,5,playerColor);
         game.loop();
@@ -73,36 +77,37 @@ var game = {
         p.y = game.mousey;
         for(i=0;i<maxEnemies;i++){
             enemies[i].x+=enemies[i].vx;
+            enemies[i].y+=enemies[i].vy;
             
             if(game.distance(enemies[i],p)<=enemies[i].rad+p.rad){
                 life --;
-                console.log(life);
+                // console.log(life);
                 enemies.splice(i,1);
                 var temp = new enemy(
-                    Math.random()*canvas.width+canvas.width,
-                    Math.random()*canvas.height,
+                    Math.random()*2*canvas.width,
+                    Math.random()*canvas.height-canvas.height,
                     Math.random()*(-5)-2,
                     0,
-                    Math.random()*3+3,
-                    enemyColor
+                    Math.random()*3+3,enemyColor
                 );
+                temp.vy = -1*temp.vx;
                 enemies.push(temp);
             }
 
-            if(enemies[i].x <= 0){
+            if(enemies[i].x <= 0 || enemies[i].y >= canvas.height){
                 enemies.splice(i,1);
                 // setTimeout( function() {
                 //     enemies.splice(i,1);
                 //   }, 0);
                 i--; 
                 var temp = new enemy(
-                    Math.random()*canvas.width+canvas.width,
-                    Math.random()*canvas.height,
+                    Math.random()*2*canvas.width,
+                    Math.random()*canvas.height-canvas.height,
                     Math.random()*(-5)-2,
                     0,
-                    Math.random()*3+3,
-                    enemyColor
+                    Math.random()*3+3,enemyColor
                 );
+                temp.vy=-1*temp.vx;
                 enemies.push(temp);
             }
         }
@@ -133,18 +138,20 @@ var game = {
         }
         c.globalAlpha = 1;
         c.fillStyle = playerColor;
+        c.shadowBlur = 10;
+        c.shadowColor = "silver";
         c.beginPath();
         c.arc(p.x,p.y,p.rad,0,Math.PI*2,false);
         c.closePath();
         c.fill();
 
-        c.beginPath();
-            c.strokeStyle = 'red';
-            c.moveTo(mousex,mousey);
-            c.lineTo(game.prevx,game.prevy);
-            c.lineWidth = 1;
-            c.closePath();
-            c.stroke();
+        // c.beginPath();
+        //     c.strokeStyle = 'red';
+        //     c.moveTo(mousex,mousey);
+        //     c.lineTo(game.prevx,game.prevy);
+        //     c.lineWidth = 1;
+        //     c.closePath();
+        //     c.stroke();
 
         // c.moveTo(p.x,p.y);
         // c.beginPath();
